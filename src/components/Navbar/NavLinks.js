529760530a56b42a9fec33616f9e47820097511e
bs01107 <debashis.suvra@brainstation-23.com>
2025-01-28 00:37:53 +0600

    schedule-meeting

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -16,9 +16,15 @@ const NavLinks = () => {
             <HashLink className="px-4 font-extrabold text-gray-500 hover:text-blue-900" to="/contact#contact">
                 Contact Us
             </HashLink>
-            <HashLink className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl" smooth to="/get-demo#demo">
-                Demo our products
-            </HashLink>
+            <a
+                href="https://cal.com/debashis123/15min"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-white bg-blue-900 hover:bg-blue-800 inline-flex items-center justify-center w-auto px-6 py-3 shadow-xl rounded-xl"
+            >
+                Schedule Meeting NOW!
+            </a>
+
         </>
     )
 }
